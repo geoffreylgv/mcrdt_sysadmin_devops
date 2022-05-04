81815879d78ce2c0102e6b6f0ff35e32f43cf69e
geoffreylgv <evanomarkez@gmail.com>
2022-05-04 15:06:28 +0000

    sysadmin util command 1/3

---
+++ network_script_utils.sh
@@ -0,0 +1,22 @@
+```bash
+$ aria2 #– downloading just about everything. Torrents included.
+$ arpwatch #– Ethernet Activity Monitor.
+$ bmon #– bandwidth monitor and rate estimator.
+$ bwm-ng #– live network bandwidth monitor.
+$ curl #– transferring data with URLs. (or try httpie)
+$ darkstat #– captures network traffic, usage statistics.
+$ dhclient #– Dynamic Host Configuration Protocol Client
+$ dig #– query DNS servers for information.
+$ dstat #– replacement for vmstat, iostat, mpstat, netstat and ifstat.
+$ ethtool #– utility for controlling network drivers and hardware.
+$ gated #– gateway routing daemon.
+$ host #– DNS lookup utility.
+$ hping #– TCP/IP packet assembler/analyzer.
+$ ibmonitor #– shows bandwidth and total data transferred.
+$ ifstat #–  report network interfaces bandwidth.
+$ iftop #– display bandwidth usage.
+$ ip (PDF file) #– a command with more features that ifconfig (net-tools).
+$ iperf3 #– network bandwidth measurement tool. (above screenshot Stacklinux VPS)
+$ iproute2 #– collection of utilities for controlling TCP/IP.
+$ iptables #– take control of network traffic.
+```
